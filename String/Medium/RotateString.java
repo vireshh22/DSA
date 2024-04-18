a88d2b87d9eaa1c260bd545236f786cdc69b86f4
vireshh112 <viresh0489@gmail.com>
2024-04-18 19:34:29 +0530

    Medium level question

---
+++ String/Medium/RotateString.java
@@ -0,0 +1,11 @@
+package String.Easy;
+
+public class RotateString {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        if(s.length() != goal.length()) System.out.println("No");
+        String concatinated = s + s;
+        System.out.println(concatinated.contains(goal));
+    }
+}
