06ea27f186d6a25a3b02cde5bb7db72fa7059741
vireshh112 <viresh0489@gmail.com>
2024-04-20 09:05:53 +0530

    Optimal solution for max product subarray

--- String/Medium/MaxProductSubArr.java
+++ String/Medium/MaxProductSubArr.java
@@ -2,16 +2,30 @@ package String.Medium;
 
 public class MaxProductSubArr {
     public static void main(String[] args) {
-        int[] arr = {-2,0,-1};
-        int n = arr.length;
-        int largest = Integer.MIN_VALUE;
+        // int[] arr = {-2,0,-1};
+        // int n = arr.length;
+        // int largest = Integer.MIN_VALUE;
+        // for(int i=0;i<n;i++){
+        //     int product=1;
+        //     for(int j=i;j<n;j++){
+        //         product *= arr[j];
+        //         largest = Math.max(largest,product);
+        //     }
+        // }
+        // System.out.println(largest);
+
+        int[] nums = {2,4,-1,3};
+        int n = nums.length;
+
+        int prefix=1, suffix=1;
+        int ans = Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            int product=1;
-            for(int j=i;j<n;j++){
-                product *= arr[j];
-                largest = Math.max(largest,product);
-            }
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n - i -1];
+            ans = Math.max(ans, Math.max(prefix,suffix));
         }
-        System.out.println(largest);
+        System.out.println(ans);
     }
 }
